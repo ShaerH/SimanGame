3232fa3d37deec1c6935fd3a3f94594f03eeec22
Shaer Hasan <shaer2m@gmail.com>
2019-03-22 15:52:21 -0400

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColor = buttonColours[nextSequence()];
 var gamePattern = [randomChosenColor];
-function nextSequence(){
+var userClickedPattern = [];
+
+
+
+function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     return randomNumber;
 }
-console.log(gamePattern);
+
+$(".btn").on("click", function (i) {
+    
+    var userChosenColor = i.target.id;
+    var sound = new Audio("sounds/"+ userChosenColor+".mp3");
+
+    userClickedPattern.push(userChosenColor);
+
+    sound.play();
+    $("#"+userChosenColor).fadeOut(100).fadeIn(100);
+
+    console.log(userClickedPattern);
+});
+
